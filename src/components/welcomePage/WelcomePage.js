d4a03cd4ec0470009e2f8300c7cc35680a0c8c83
HoxtonR <61260496+HoxtonR@users.noreply.github.com>
2022-04-19 14:55:05 -0400

    gitinitproject

---
+++ src/components/welcomePage/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WelcomePage.css';
+
+export default class WelcomePage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
+}
