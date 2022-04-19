021b4ed2fa3228c31c81c8ef21f14d4063bd7b2c
HoxtonR <61260496+HoxtonR@users.noreply.github.com>
2022-04-19 16:53:48 -0400

    fix some bugs

---
+++ src/components/nevigationBar/NevigationBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+export default class NevigationBar extends Component {
+    showSettings (event) {
+        event.preventDefault();
+ 
+      }
+  render() {
+    return (
+        <div>
+
+        </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import WelcomePage from './components/welcomePage/WelcomePage.js';
-
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <WelcomePage />
+    <App />
 );
 
