da361cf04bf7ee870f2aaa667b3ccfa28c097b1b
Rekordny <loadingwheel@gmail.com>
2022-04-19 18:39:04 -0300

    Adjustment to sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import WelcomePage from './components/welcomePage/WelcomePage'
-import NevigationBar from './components/nevigationBar/NevigationBar'
+import NevigationBar from './components/nevigationBar/NevigationBar.jsx'
+import './App.css';
 export default class App extends Component {
   render() {
     return (
-      <div>
-          <WelcomePage />
-          
+      <div id = "main-container">
+          <NevigationBar id = "main-sidebar"/>
+          <WelcomePage id = "main-contents"/>
       </div>
     )
   }
--- src/components/nevigationBar/NevigationBar.jsx
+++ src/components/nevigationBar/NevigationBar.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import './NevigationBar.css'
+
 export default class NevigationBar extends Component {
-    showSettings (event) {
-        event.preventDefault();
- 
-      }
+
+  showHome(event){
+    event.preventDefault();
+  }
+
+  showFocus(event){
+    event.preventDefault();
+  }
+
+  showIdea(event){
+    event.preventDefault();
+  }
+
+  showAbout(event){
+    event.preventDefault();
+  }
+
+  showSettings (event) {
+    event.preventDefault();
+    alert("Setting clicked");
+  }
+
   render() {
     return (
-        <div>
-
-        </div>
+      <Menu>
+        <a id="home" className="menu-item" href="/">HOME</a>
+        <a id="focus" className="menu-item" href="/about">FOCUS</a>
+        <a id="idea" className="menu-item" href="/about">IDEA</a>
+        <a id="about" className="menu-item" href="/contact">ABOUT</a>
+        <a onClick={ this.showSettings } className="menu-item" href="">SETTINGS</a>
+      </Menu>
     )
   }
 }
